c4cf48f8d3c01e8ba7ba17e436399546e4c04021
Alexandra Farina <alexandra.farina@ynov.com>
2023-11-17 15:22:08 +0100

    🚀  Init project

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: [
+    '@react-native',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+};
